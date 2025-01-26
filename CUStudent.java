ee5a83f05cb7e7669e3f6034bc670402fd40b4e7
QUEENDOLIN-EFFA ETTA <158026407+queendolin12@users.noreply.github.com>
2025-01-26 17:55:17 +0100

    Add files via upload
    Java Class Practice

---
+++ CUStudent.java
@@ -0,0 +1,12 @@
+import Java.io.*;
+ public Class CUStudent (){
+    private char[] FirstName;
+    private char[] LastName;
+    private int Age;
+    private float CGPA;
+    private float chapelAttendance;   
+    
+   
+}
+
+}
